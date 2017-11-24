1d38599b5ceef9124187d34b19dc8b0bd9ffc87b
ShikharB92 <33369656+ShikharB92@users.noreply.github.com>
2017-11-24 10:24:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.post('/webhook', (req, res) => {
  
   let body = req.body;
   console.log("ID: "+ req.body);
+  console.log("ID: "+ entry.messaging[0].sender.id);
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
